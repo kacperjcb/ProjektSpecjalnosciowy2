63e437c0f7f71099b8e1c35f095f641660d5d306
kacperjcb <kacperjcb@gmail.com>
2023-05-10 09:02:23 +0200

    change annotations to attributes and add getClientById and getCrudById functions in ApiController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Controller;
 
 use App\Entity\Crud;
@@ -13,28 +14,35 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+
 #[Route('/api')]
 class ApiController extends AbstractController
 {
-    
-     
-/**
-     * @Route("/client", methods={"GET"})
-     */
+
+
+    #[Route("/client", methods: ["GET"])]
     public function getClientInfo(EntityManagerInterface $entityManager): Response
     {
         $clientinfo = $entityManager->getRepository(ClientInfo::class)->findAll();
 
         return $this->json($clientinfo);
     }
- 
-    /**
-     * @Route("/client", methods={"POST"})
-     */
+    #[Route("/client/{id}", methods: ["GET"])]
+    public function getClientById(EntityManagerInterface $entityManager, $id): Response
+    {
+        $clientinfo = $entityManager->getRepository(ClientInfo::class)->find($id);
+
+        if (!$clientinfo) {
+            return new JsonResponse(['error' => 'Crud info not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($clientinfo);
+    }
+    #[Route("/client", methods: ["POST"])]
     public function createClientInfo(ManagerRegistry $doctrine, Request $request)
     {
         $data = json_decode($request->getContent(), true);
-        
+
         $clientInfo = new ClientInfo();
         $clientInfo->setName($data['Name']);
         $clientInfo->setSurname($data['Surname']);
@@ -42,92 +50,94 @@ class ApiController extends AbstractController
         $clientInfo->setPostCode($data['PostCode']);
         $clientInfo->setAddress($data['Address']);
         $clientInfo->setOrderNumber($data['OrderNumber']);
-    
+
         $entityManager = $doctrine->getManager();
         $entityManager->persist($clientInfo);
         $entityManager->flush();
-    
+
         return new JsonResponse($clientInfo, JsonResponse::HTTP_CREATED);
     }
-     /**
-     * @Route("/client/{id}", methods={"PUT"})
-     */
-    public function replaceClientInfo(ManagerRegistry $doctrine,Request $request, $id)
-{
-    $data = json_decode($request->getContent(), true);
-    
-    $entityManager = $doctrine->getManager();
-    $clientInfo = $entityManager->getRepository(ClientInfo::class)->find($id);
-    
-    if (!$clientInfo) {
-        return new JsonResponse(['error' => 'Client info not found'], JsonResponse::HTTP_NOT_FOUND);
+    #[Route("/client/{id}", methods: ["PUT"])]
+    public function replaceClientInfo(ManagerRegistry $doctrine, Request $request, $id)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $entityManager = $doctrine->getManager();
+        $clientInfo = $entityManager->getRepository(ClientInfo::class)->find($id);
+
+        if (!$clientInfo) {
+            return new JsonResponse(['error' => 'Client info not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $clientInfo->setName($data['Name']);
+        $clientInfo->setSurname($data['Surname']);
+        $clientInfo->setCity($data['City']);
+        $clientInfo->setPostCode($data['PostCode']);
+        $clientInfo->setAddress($data['Address']);
+        $clientInfo->setOrderNumber($data['OrderNumber']);
+
+        $entityManager->flush();
+
+        return new JsonResponse($clientInfo);
     }
 
-    $clientInfo->setName($data['Name']);
-    $clientInfo->setSurname($data['Surname']);
-    $clientInfo->setCity($data['City']);
-    $clientInfo->setPostCode($data['PostCode']);
-    $clientInfo->setAddress($data['Address']);
-    $clientInfo->setOrderNumber($data['OrderNumber']);
+    #[Route("/client/{id}", methods: ["PATCH"])]
+    public function updateClientInfo(ManagerRegistry $doctrine, Request $request, $id)
+    {
+        $data = json_decode($request->getContent(), true);
 
-    $entityManager->flush();
+        $entityManager = $doctrine->getManager();
+        $clientInfo = $entityManager->getRepository(ClientInfo::class)->find($id);
 
-    return new JsonResponse($clientInfo);
-}
+        if (!$clientInfo) {
+            return new JsonResponse(['error' => 'Client info not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
 
- /**
-     * @Route("/client/{id}", methods={"PATCH"})
-     */
-public function updateClientInfo(ManagerRegistry $doctrine,Request $request, $id)
-{
-    $data = json_decode($request->getContent(), true);
-    
-    $entityManager = $doctrine->getManager();
-    $clientInfo = $entityManager->getRepository(ClientInfo::class)->find($id);
-    
-    if (!$clientInfo) {
-        return new JsonResponse(['error' => 'Client info not found'], JsonResponse::HTTP_NOT_FOUND);
+        $clientInfo->setName($data['Name'] ?? $clientInfo->getName());
+        $clientInfo->setSurname($data['Surname'] ?? $clientInfo->getSurname());
+        $clientInfo->setCity($data['City'] ?? $clientInfo->getCity());
+        $clientInfo->setPostCode($data['PostCode'] ?? $clientInfo->getPostCode());
+        $clientInfo->setAddress($data['Address'] ?? $clientInfo->getAddress());
+        $clientInfo->setOrderNumber($data['OrderNumber'] ?? $clientInfo->getOrderNumber());
+
+        $entityManager->flush();
+
+        return new JsonResponse($clientInfo);
     }
+    #[Route("/client/{id}", methods: ["DELETE"])]
 
-    $clientInfo->setName($data['Name'] ?? $clientInfo->getName());
-    $clientInfo->setSurname($data['Surname'] ?? $clientInfo->getSurname());
-    $clientInfo->setCity($data['City'] ?? $clientInfo->getCity());
-    $clientInfo->setPostCode($data['PostCode'] ?? $clientInfo->getPostCode());
-    $clientInfo->setAddress($data['Address'] ?? $clientInfo->getAddress());
-    $clientInfo->setOrderNumber($data['OrderNumber'] ?? $clientInfo->getOrderNumber());
+    public function deleteClientInfo(ManagerRegistry $doctrine, ClientInfo $clientInfo): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($clientInfo);
+        $entityManager->flush();
 
-    $entityManager->flush();
+        return $this->json([
+            'Client' => "Deleted",
+        ]);
+    }
+    //API FOR CRUD IS STARTING HERE
 
-    return new JsonResponse($clientInfo);
-}
-/**
- * @Route("/client/{id}", name="delete_clientinfo", methods={"DELETE"})
- */
-public function deleteClientInfo(ManagerRegistry $doctrine,ClientInfo $clientInfo): Response
-{
-    $entityManager = $doctrine->getManager();
-    $entityManager->remove($clientInfo);
-    $entityManager->flush();
-    
-    return $this->json([
-        'Client' => "Deleted",
-    ]); 
-}
-//API FOR CRUD Starting here
-   /**
-     * @Route("/crud", methods={"GET"})
-     */
+    #[Route("/crud", methods: ["GET"])]
     public function getCrudInfo(EntityManagerInterface $entityManager): Response
     {
         $crud = $entityManager->getRepository(Crud::class)->findAll();
 
         return $this->json($crud);
     }
- 
+    #[Route("/crud/{id}", methods: ["GET"])]
+    public function getCrudById(EntityManagerInterface $entityManager, $id): Response
+    {
+        $crud = $entityManager->getRepository(Crud::class)->find($id);
 
-    /**
-     * @Route("/crud", methods={"POST"})
-     */
+        if (!$crud) {
+            return new JsonResponse(['error' => 'Crud info not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($crud);
+    }
+
+    #[Route("/crud", methods: ["POST"])]
     public function createCrudInfo(ManagerRegistry $doctrine, Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -145,9 +155,8 @@ public function deleteClientInfo(ManagerRegistry $doctrine,ClientInfo $clientInf
         return new JsonResponse($crud, JsonResponse::HTTP_CREATED);
     }
 
-    /**
-     * @Route("/crud/{id}", methods={"PUT"})
-     */
+
+    #[Route("/crud/{id}", methods: ["PUT"])]
     public function replaceCrudInfo(ManagerRegistry $doctrine, Request $request, $id): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -169,9 +178,7 @@ public function deleteClientInfo(ManagerRegistry $doctrine,ClientInfo $clientInf
         return new JsonResponse($crud);
     }
 
-    /**
-     * @Route("/crud/{id}", methods={"PATCH"})
-     */
+    #[Route("/crud/{id}", methods: ["PATCH"])]
     public function updateCrudInfo(ManagerRegistry $doctrine, Request $request, $id): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
@@ -203,17 +210,16 @@ public function deleteClientInfo(ManagerRegistry $doctrine,ClientInfo $clientInf
 
         return new JsonResponse($crud);
     }
-    /**
- * @Route("/crud/{id}", name="delete_crudinfo", methods={"DELETE"})
- */
-public function deleteCrud(ManagerRegistry $doctrine,Crud $crud): Response
-{
-    $entityManager = $doctrine->getManager();
-    $entityManager->remove($crud);
-    $entityManager->flush();
-    
-    return $this->json([
-        'Client' => "Deleted",
-    ]); 
-}
+
+    #[Route("/crud/{id}", methods: ["DELETE"])]
+    public function deleteCrud(ManagerRegistry $doctrine, Crud $crud): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($crud);
+        $entityManager->flush();
+
+        return $this->json([
+            'Product' => "Deleted",
+        ]);
+    }
 }
