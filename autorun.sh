998abcd8dc90e6318ec02ac0061c50b3ab48a91e
kacperjcb <kacperjcb@gmail.com>
2023-10-05 17:07:48 +0200

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone -b DockerCompose https://github.com/kacperjcb/ProjektSpecjalnosciowy2 && cd ProjektSpecjalnosciowy2 && docker-compose up -d && sleep 15 && docker-compose exec php composer install
+docker-compose up -d && sleep 15 && docker-compose exec php composer install
 
